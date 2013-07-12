99d3206c5527771a172beb22be394e6e2ff4eed0
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-12 08:27:21 +0000

    CP Image Store is an online store for the sale of image files: images, predefined pictures, clipart, drawings, vector images. For payment processing, CP Image Store uses PayPal, which is the most widely used payment gateway, safe and easy to use.
    git-svn-id: https://plugins.svn.wordpress.org/cp-image-store/tags/1.0.1@739664 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/admin.js
@@ -0,0 +1,105 @@
+﻿jQuery(function(){
+	(function($){
+		// Methods definition
+		window['cpis_remove'] = function(e){
+            $(e).parents('.cpis-property-container').remove();
+        };
+		
+		window['cpis_select_element'] = function(e, add_to, new_element_name){
+			var v = e.options[e.selectedIndex].value,
+				t = e.options[e.selectedIndex].text;
+			if(v != 'none'){
+				$('#'+add_to).append(
+					'<div class="cpis-property-container"><input type="hidden" name="'+new_element_name+'[]" value="'+v+'" /><input type="button" onclick="cpis_remove(this);" class="button" value="'+t+' [x]"></div>'
+				);
+			}	
+		};
+		
+		window['cpis_add_element'] = function(input_id, add_to, new_element_name){
+			var n = $('#'+input_id),
+				v = n.val();
+			n.val('');	
+			if( !/^\s*$/.test(v)){
+				$('#'+add_to).append(
+					'<div class="cpis-property-container"><input type="hidden" name="'+new_element_name+'[]" value="'+v+'" /><input type="button" onclick="cpis_remove(this);" class="button" value="'+v+' [x]"></div>'
+				);
+			}	
+		};
+		
+		window['cpis_delete_purchase'] = function(id){
+			if(confirm('Are you sure to delete the purchase record?')){
+				var f = $('#purchase_form');
+				f.append('<input type="hidden" name="purchase_id" value="'+id+'" />');
+				f[0].submit();
+			}	
+		};
+		
+        window["cpis_new_file"] = function(){
+            var nTr =   ' \
+                            <tr> \
+                                <td style="width:60%;"><input type="file" name="cpis_file_new[]" style="width:100%;" /></td> \
+                                <td style="width:10%;"><input type="text" name="cpis_file_width_new[]" class="cpis-short-field" /></td>     \
+                                <td style="width:10%;"><input type="text" name="cpis_file_height_new[]" class="cpis-short-field" /></td>    \
+                                <td style="width:10%;"><input type="text" name="cpis_file_price_new[]" class="cpis-short-field" /></td>     \
+                                <td style="width:10%;"> <input type="button" onclick="cpis_remove_file(this);" value="Remove" /></td> \
+                            </tr> \
+                        ';
+            
+            $('.cpis-file-list').append(nTr);
+        };
+
+        window["cpis_remove_file"] = function( e, id ){
+            e = $(e);
+            if(id){
+                if( e.data( 'processing' ) == 1) return;
+                e.data( 'processing', 1 );
+                
+                if( image_store && image_store.hurl ){
+                    e.parents('tr').fadeTo( 'normal', 0.5 );
+                    $.getJSON( 
+                        image_store.hurl+'wp-admin/?cpis-action=remove-image&image='+id, 
+                        function( data ){
+                            if( data.error){ 
+                                alert( data.error );
+                                e.parents('tr').fadeTo( 'normal', 1 );
+                                e.data( 'processing', 0 );
+                            }else{
+                                e.parents('tr').remove();
+                            }    
+                        }
+                    );
+                }
+            }else{
+                e.parents('tr').remove();
+            }
+        };        
+
+        window ['cpis_insert_store'] = function(){
+            if(send_to_editor) send_to_editor('[codepeople-image-store]');
+		};
+		
+        window ['cpis_insert_product_window'] = function(){
+        	var tags = ( image_store ) ? image_store.tags : '',
+				cont = $(tags.replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '"'));
+			
+			cont.dialog({
+				dialogClass: 'wp-dialog',
+				modal: true,
+				closeOnEscape: true,
+                close: function(){
+                    $(this).remove();
+                },
+				buttons: [
+					{text: 'OK', click: function() {
+						var id  = $('#product_id').val(),
+                            l   = $('#layout').val(),
+							sc  = '[codepeople-image-store-product id="'+id+'" layout="'+l+'"]';
+
+						if(send_to_editor) send_to_editor(sc);
+						$(this).dialog("close"); 
+					}}
+				]
+			});
+		};
+    })(jQuery)
+})
---
+++ js/public.js
@@ -0,0 +1,124 @@
+﻿(function($){
+	var cpis_mousemove = function( e ) {
+        if( 
+            e.pageX < e.data.x1 ||
+            e.pageX > e.data.x2 ||
+            e.pageY < e.data.y1 ||
+            e.pageY > e.data.y2 
+        ){
+            $( document ).unbind( 'mousemove', cpis_mousemove );
+            $( '.cpis-image-data' ).hide();
+        }    
+    };
+    
+	window[ 'cpis_display_dlg' ] = function( e, id, incorner ){
+    
+        // Close all image data panels
+        $( '.cpis-image-data' ).hide();
+        
+        var d = $( '#cpis_image'+id );
+        
+        e = $( e );
+        
+        if(d.length){
+            if( incorner ){
+                d.show().position(
+                    {
+                        my: "left top",
+                        at: "left top",
+                        of: e
+                    }
+                );
+            }else{
+                d.show().position(
+                    {
+                        my: "center",
+                        at: "center",
+                        of: e
+                    }
+                );
+            }
+            
+        
+            var o = d.offset();
+            $( document ).bind( 
+                'mousemove', 
+                {
+                    x1: o.left,
+                    y1: o.top,
+                    x2: o.left + d.width(),
+                    y2: o.top + d.height()
+                },
+                cpis_mousemove
+            );
+        }    
+    };
+    
+    window[ 'cpis_buynow' ] = function( e ){
+        if( $( e ).parents( 'form' ).find( 'input:checked' ).length ) e.form.submit();
+        else if( image_store && image_store[ 'file_required_str' ] ) alert( image_store[ 'file_required_str' ] );
+    };
+    
+    // Main Code
+    $(
+        function(){
+    
+            // Carousel routines
+            
+            // Set carousel
+            
+            var c = $('[id="cpis-image-store-carousel"]');
+            if( c.length ){
+                var li = c.find( 'li' );
+                if( li.length ){
+                    var l = $('<div class="cpis-carousel-left" style="" ></div>'),
+                        r = $('<div class="cpis-carousel-right" style="" ></div>'),
+                        i = c.find( '.cpis-carousel-container' ),
+                        u = $(c.find('ul').get(0));
+                    
+                    if( image_store && image_store[ 'thumbnail_h' ] ){    
+                        l.css( 'top', ( ( image_store[ 'thumbnail_h' ]-60 ) / 2) + 'px' );
+                        r.css( 'top', ( ( image_store[ 'thumbnail_h' ]-60 ) / 2) + 'px' );
+                    }
+                    i.width( c.width() -  42 );
+                    c.append( l ).append( r );
+                    var _auto = false;
+                    if( image_store && image_store[ 'carousel_autorun' ] != 0){
+                        _auto = { pauseOnHover: 'resume' };
+                    }            
+                    
+                    u.carouFredSel({
+                        width: '100%',
+                        height: 'auto',
+                        prev: '.cpis-carousel-left',
+                        next: '.cpis-carousel-right',
+                        auto: _auto
+                    });
+                        
+                }
+            }
+            
+            // Set license handle
+            $( '.cpis-license-title.cpis-link' ).click(
+                function(){
+                    var e = $( this );
+                    $( '.cpis-license-container' )
+                        .toggle()
+                        .position(
+                            {
+                                my: 'left top',
+                                at: 'left top',
+                                of: e
+                            }
+                        );
+                }
+            );
+            
+            $( '.cpis-license-close' ).click( 
+                function(){
+                    $( '.cpis-license-container' ).hide();
+                } 
+            );
+        }
+    );    
+})(jQuery);
